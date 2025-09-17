e6502e3c06c1b1d654c932396fbcdc009ec092c1
Andrew <you@example.com>
2025-09-17 15:09:35 +0700

    JS can to add and remove record

--- js/index.js
+++ js/index.js
@@ -1,68 +1,104 @@
 const btnAdd = document.querySelector('.btn-add')
 const btnGenerate = document.querySelector('.btn-generate')
 
+const inputUrl = document.getElementById('url')
 const inputLogin = document.getElementById('login')
 const inputPassw = document.getElementById('password')
-const inputUrl = document.getElementById('url')
+
+const attentMesg = document.querySelector('.attention-message')
 
 const savedSites = document.querySelector('.saved-sites')
 
-// const obj = {
-// 	login: '123',
-// 	pass: 'pass',
-// }
-
-// // localStorage.setItem('web', JSON.stringify(obj))
-
-// const raw = localStorage.getItem('web')
-// const saveInfo = JSON.parse(raw)
-
-// saveInfo.login = 'Andrew_man'
-
-// console.log(saveInfo)
-
-btnGenerate.addEventListener('click', e => {})
-
-btnAdd.addEventListener('click', e => {
-	let logValue = inputLogin.value
-	let passValue = inputPassw.value
-	let urlValue = inputUrl.value
-
-	const complexSite = {
-		url: urlValue,
-		login: logValue,
-		password: passValue
-	}
-
-	localStorage.setItem(urlValue, JSON.stringify(complexSite))
-
-	location.reload()
-})
-
-function displayLocalStorage() {
-	
-	const keys = Object.keys(localStorage);
-
-	keys.forEach(key =>{
-		const value = localStorage.getItem(key);
-		const sitesInDOM = document.createElement('div')
-		sitesInDOM.classList.add('box-for-sites')
-		sitesInDOM.innerText = ` ${value}`
-		savedSites.appendChild(sitesInDOM)
+function generatePassword() {
+	btnGenerate.addEventListener('click', e => {
+		inputPassw.innerHTML = ''
+		var randomstring = Math.random().toString(36).slice(-8)
+		inputPassw.value = randomstring
+		console.log(inputPassw.value)
 	})
-	
-	
-
 }
 
-displayLocalStorage();
+function addRecord() {
+	btnAdd.addEventListener('click', e => {
+		let logValue = inputLogin.value
+		let passValue = inputPassw.value
+		let urlValue = inputUrl.value
 
+		const complexSite = {
+			url: urlValue,
+			login: logValue,
+			password: passValue,
+		}
 
-// let keys = Object.keys(localStorage)
+		if (
+			complexSite.url === '' ||
+			complexSite.password === '' ||
+			complexSite.login === ''
+		) {
+			attentMesg.innerText = 'Please fill in all input fields!'
+		} else {
+			localStorage.setItem(urlValue, JSON.stringify(complexSite))
+			attentMesg.innerHTML = ''
+			location.reload()
+		}
+	})
+}
 
-// if (keys.length) {
-// 	savedSites.textContent("There's nothing now. ")
-// }
+function displayLocalStorage() {
+	const keys = Object.keys(localStorage)
+
+	keys.forEach(key => {
+		const value = localStorage.getItem(key)
+		const parsedValue = JSON.parse(value)
+
+		const buttonCopy = document.createElement('button')
+		buttonCopy.classList.add('button-for-box-copy')
+		buttonCopy.innerText = `Copy`
+
+		const buttonDelete = document.createElement('button')
+		buttonDelete.classList.add('button-for-box-delete')
+		buttonDelete.innerText = `Delete`
+
+		const sitesInDOM = document.createElement('div')
+		sitesInDOM.classList.add('box-for-sites')
+		sitesInDOM.innerText = `url : ${parsedValue.url}
+		login : ${parsedValue.login}
+		password : ${parsedValue.password}
+		`
+
+		buttonCopy.addEventListener('click', () => {
+			const textToCopy = `url : ${parsedValue.url}
+			login : ${parsedValue.login}
+			password : ${parsedValue.password}
+			`
+
+			if (navigator.clipboard && navigator.clipboard.writeText) {
+				navigator.clipboard
+					.writeText(textToCopy)
+					.then(() => {
+						alert('Текст успешно скопирован!')
+					})
+					.catch(err => {
+						console.error('Ошибка копирования: ', err)
+					})
+			}
+		})
+
+		buttonDelete.addEventListener('click', () => {
+			localStorage.removeItem(key)
+			location.reload()
+		})
+
+		savedSites.appendChild(sitesInDOM)
+		savedSites.appendChild(buttonCopy)
+		savedSites.appendChild(buttonDelete)
+	})
+}
+
+displayLocalStorage()
+
+addRecord()
+generatePassword()
 
 // window.addEventListener('storage', event => {
 // 	console.log(event)
